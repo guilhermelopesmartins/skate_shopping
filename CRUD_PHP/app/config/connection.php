32a97bc76b18f3debef2c5a85e73e96ef270d711
Guilherme Lopes Martins <guirockmartins@hotmail.com>
2023-03-06 20:52:00 -0300

    Iniciando projeto

---
+++ CRUD_PHP/app/config/connection.php
@@ -0,0 +1,10 @@
+<?php
+    class Connection {
+        public $host = '';
+        public $dbname = '';
+        public $port = ''
+        public $user = '';
+        public $password = '';
+        
+    }
+?>
